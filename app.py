7bcd756af5bccf5cf25dfa25f0c4cba8aa1876d0
Zuli Amrullah <zulyazqall@gmail.com>
2026-06-15 12:32:26 +0900

    update port

--- app.py
+++ app.py
@@ -789,4 +789,4 @@ if __name__ == "__main__":
         save_config(initial_config)
 
     #app.run(debug=True)
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=4000, debug=True)
